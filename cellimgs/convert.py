02045f9a1af67db9e3483f57a45881018d138ec5
bhalliga <bhalliga@med.umich.edu>
2024-01-23 10:35:49 -0500

    removed click in favor of argparse

--- cellimgs/convert.py
+++ cellimgs/convert.py
@@ -3,7 +3,8 @@ import os
 import re
 import glob
 
-import click
+import argparse
+
 import pandas as pd
 import numpy as np
 
@@ -16,24 +17,30 @@ import bioformats
 
 from .logger import logger
 
-@click.command()
-@click.argument('indir')
-@click.argument('outdir')
-@click.option('--channel','-c', default='*', help='Specific Channel')
-def convert(indir, outdir, channel):
+parser = argparse.ArgumentParser(description="Convert CO1 images to tifs")
+
+# required arguments, input dir and dest dir
+parser.add_argument("indir", type=str, help='CO1 folder')
+parser.add_argument("outidr", type=str, help="Tif folder")
+
+# Optional argumnets
+parser.add_argument("--channel", "-c", default="*", type=str, help="Specific channel")
+args = parser.parse_args()
+
+def convert():
     if os.name =='nt':
         os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-    assert os.path.exists(indir), "C01 Directory doesn't exist"
-    assert os.path.exists(outdir), "TIF directory doens't exist"
+    assert os.path.exists(args.indir), "C01 Directory doesn't exist"
+    assert os.path.exists(args.outdir), "TIF directory doens't exist"
 
-    logger(outdir, locals())
+    logger(args.outdir, locals())
 
     javabridge.start_vm(class_path=bioformats.JARS)
-    path = os.path.join(indir, f'*{channel}.C01')
+    path = os.path.join(args.indir, f'*{args.channel}.C01')
     files = glob.glob(path)
     bar = Bar('converting',max=len(files))
     for f in files:
-        filename = os.path.join(outdir,re.search(r'(MFGTMP_\d+_\w+)', f, re.IGNORECASE).group(1)+'.tif')
+        filename = os.path.join(args.outdir,re.search(r'(MFGTMP_\d+_\w+)', f, re.IGNORECASE).group(1)+'.tif')
         data = bioformats.ImageReader(f).read()
         # data = data.astype('float16')
         tif.imsave(filename, data=data)
